9e46e381068b1e23ea0f08343ef439de7354e59c
Luke Lalor <lukehlalor@gmail.com>
2023-11-10 16:10:14 -0800

    fill in agent_os interface

--- sdk/sdk/agent_os.py
+++ sdk/sdk/agent_os.py
@@ -1,53 +1,50 @@
-from abc import ABC
+from typing import TypeVar, Generic
+
 from fastapi import Request
 from fastapi.openapi.models import Response
 
+T = TypeVar('T')
+V = TypeVar('V')
 
-class IOUnit:
+
+class Agent:
+    def config(self):
+        pass
+
+    def agent_memory(self):
+        pass
+
+
+class IOUnit(Generic[T, V]):
     @staticmethod
-    def read(self, request: Request):
+    async def read(request: Request) -> T:
         pass
 
     @staticmethod
-    def write(data, response: Response):
+    async def write(data: V, response: Response) -> None:
         pass
 
 
 class LogicUnit:
     @staticmethod
-    def execute(instruction):
+    async def execute(instruction, agent: Agent) -> None:
+        # unsure of the object we pass of to manipulate framework. Calling it agent for now
         pass
 
 
 class MemoryUnit:
     @staticmethod
-    def load(address):
-        pass
-
-    @staticmethod
-    def store(address, data):
+    async def process(prompt, agent: Agent) -> None:
         pass
 
 
-class ControlUnit(ABC):
+class ControlUnit(Generic[T, V]):
     @staticmethod
-    def fetch(self):
-        pass
-
-    @staticmethod
-    def decode(instruction):
-        pass
-
-    @staticmethod
-    def execute(decoded_instruction):
+    async def process(request: T, agent: Agent) -> V:
         pass
 
 
 class LLMUnit:
     @staticmethod
-    def query(prompt):
-        pass
-
-    @staticmethod
-    def respond(prompt):
+    async def query(prompt):
         pass
