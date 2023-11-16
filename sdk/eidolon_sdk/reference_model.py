6e108654aded5a29670e56fc71cc7d68d0d90750
Luke Lalor <lukehlalor@gmail.com>
2023-11-15 22:38:42 -0800

    make reference model a more forgiving mixin

--- sdk/eidolon_sdk/reference_model.py
+++ sdk/eidolon_sdk/reference_model.py
@@ -9,11 +9,9 @@ from eidolon_sdk.util.class_utils import for_name
 T = typing.TypeVar('T', bound=BaseModel)
 
 
-class Plugable(typing.Generic[T]):
+# Type wrapper to describe the config type which is expected. If not specable, spec is passed in un-validated
+class Specable(typing.Generic[T]):
     spec: T
-
-    def __init__(self, *args, **kwargs):
-        self.spec = kwargs['spec']
 
 
 class ReferenceMeta(type):
@@ -25,22 +23,18 @@ class ReferenceMeta(type):
 
             @model_validator(mode='after')
             def _validate(self):
-                if not issubclass(self._sub_class, Plugable):
-                    raise ValueError(f"Implementation class '{self.implementation}' is not a Plugable.")
-                self._build_spec(for_name(self.implementation, self._sub_class))
+                self.build_reference_spec()
                 return self
 
-            def instantiate(self):
-                impl_class = for_name(self.implementation, self._sub_class)
-                return impl_class(spec=self._build_spec(impl_class))
-
-            def _build_spec(self, impl_class):
-                # todo, this needs to be more robust
-                bases = getattr(impl_class, '__orig_bases__', None)
-                if not bases:
-                    raise ValueError(f"Unable to find config object")
-                spec_type, = bases[0].__args__
-                return spec_type.model_validate(self.spec)
+            def build_reference_spec(self):
+                if issubclass(self._sub_class, Specable):
+                    bases = getattr(for_name(self.implementation, self._sub_class), '__orig_bases__', None)
+                    if not bases:
+                        raise ValueError(f"Unable to find config object")
+                    spec_type, = bases[0].__args__
+                    return spec_type.model_validate(self.spec)
+                else:
+                    return self.spec
 
         return GenericReference
 
@@ -48,5 +42,5 @@ class ReferenceMeta(type):
 class Reference(metaclass=ReferenceMeta):
     pass
 
-    def instantiate(self):
+    def build_reference_spec(self):
         pass
