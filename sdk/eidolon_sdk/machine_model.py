517908477961c6d258568640877e219b0338dbc0
Luke Lalor <lukehlalor@gmail.com>
2023-11-16 01:32:02 -0800

    move cpu model back in since it does not need to split out

--- sdk/eidolon_sdk/machine_model.py
+++ sdk/eidolon_sdk/machine_model.py
@@ -6,7 +6,8 @@ from pydantic import Field, BaseModel
 
 from .agent import Agent
 from .agent_memory import FileMemory, SymbolicMemory, SimilarityMemory
-from .cpu_model import CpuModel
+from .cpu.agent_io import IOUnit
+from .cpu.memory_unit import MemoryUnit
 from .reference_model import Reference
 
 
@@ -15,10 +16,15 @@ class MachineModel(BaseModel):
     agent_programs: List[ProgramModel] = Field(description="The list of Agent Programs to run on this machine.")
 
 
+class CpuModel(BaseModel):
+    memory_unit: Reference[MemoryUnit] = Reference[MemoryUnit](implementation='eidolon_sdk.cpu.memory_unit.MemoryUnit')
+    io_unit: Reference[IOUnit] = Reference[IOUnit](implementation='eidolon_sdk.cpu.agent_io.IOUnit')
+
+
 class ProgramModel(BaseModel):
     name: str = Field(description="The name of the program.")
     agent: Reference[Agent] = Field(description="The Agent implementation to use.")
-    cpu: CpuModel = CpuModel()
+    cpu: CpuModel = Field(CpuModel(), description="The CPU implementation to use.")
 
 
 class MemoryModel(BaseModel):
