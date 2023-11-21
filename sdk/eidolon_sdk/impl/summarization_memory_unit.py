65a02c616364d31b0ec705c2e54a094f9b9af52d
Eric Brewster <54458170+TheSheepGoesBa@users.noreply.github.com>
2023-11-21 02:17:48 -0500

    Max token frac in summarization memory config

--- sdk/eidolon_sdk/impl/summarization_memory_unit.py
+++ sdk/eidolon_sdk/impl/summarization_memory_unit.py
@@ -1,15 +1,24 @@
-from typing import List
+from typing import List, Annotated
 
 import tiktoken
+from pydantic import Field
 
 from eidolon_sdk.cpu.agent_bus import CallContext
 from eidolon_sdk.cpu.llm_message import LLMMessage
+from eidolon_sdk.cpu.llm_unit import LLM_MAX_TOKENS
 from eidolon_sdk.cpu.memory_unit import MemoryUnit, MemoryUnitConfig
 from eidolon_sdk.reference_model import Specable
-from eidolon_sdk.cpu.llm_unit import LLM_MAX_TOKENS
+
+
+class SummarizationMemoryUnitConfig(MemoryUnitConfig):
+    max_token_frac: Annotated[float, Field(strict=True, gt=0, le=1)] = 0.75
 
 
 class SummarizationMemoryUnit(MemoryUnit, Specable[MemoryUnitConfig]):
+    def __init__(self, spec: SummarizationMemoryUnitConfig):
+        super().__init__(spec)
+        self.max_token_frac = spec.max_token_frac
+
     async def writeMessages(self, call_context: CallContext, messages: List[LLMMessage]):
         conversationItems = [{
             "process_id": call_context.process_id,
@@ -34,9 +43,8 @@ class SummarizationMemoryUnit(MemoryUnit, Specable[MemoryUnitConfig]):
         return existingMessages
 
     async def processStoreAndFetchEvent(self, call_context: CallContext, messages: List[LLMMessage]):
-        SUMMARIZE_FRAC = 0.75
         # cl100k_base encodings only work for gpt-3.5-turbo and up models
-        if sum(len(tiktoken.get_encoding('cl100k_base').encode(message.text)) for message in messages) > LLM_MAX_TOKENS.get('gpt-4-1106-preview') * SUMMARIZE_FRAC:
+        if sum(len(tiktoken.get_encoding('cl100k_base').encode(message.text)) for message in messages) >= LLM_MAX_TOKENS.get('gpt-4-1106-preview') * self.max_token_frac:
             # call summarizer
             pass
         await super().processStoreAndFetchEvent(call_context, messages)
