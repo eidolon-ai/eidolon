f91aaee8463bbdc70ddd1c84ecebc1ba88c9a43f
Luke Lalor <lukehlalor@gmail.com>
2023-11-28 23:42:40 -0800

    make tot pluggable

--- sdk/eidolon_sdk/impl/tot_controller/checker.py
+++ sdk/eidolon_sdk/impl/tot_controller/checker.py
@@ -48,7 +48,7 @@ class ToTChecker(Specable[TotCheckerConfig]):
 
         resp = await self.cpu.process_llm_requests(
             context,
-            prior_messages + [UserMessage(content=[UserMessageText(text=checker_prompt)])],
+            [UserMessage(content=[UserMessageText(text=checker_prompt)])],
             False,
             ThoughtValidity.model_json_schema()
         )
--- sdk/eidolon_sdk/impl/tot_controller/tot_agent.py
+++ sdk/eidolon_sdk/impl/tot_controller/tot_agent.py
@@ -3,10 +3,12 @@ from textwrap import indent
 from typing import List, Dict, Any, Literal
 
 from fastapi import HTTPException
+from jinja2 import StrictUndefined, Environment
 from pydantic import Field, BaseModel
 
 from eidolon_sdk.agent import initializer, Agent
 from eidolon_sdk.cpu.agent_io import UserTextCPUMessage
+from eidolon_sdk.cpu.call_context import CallContext
 from eidolon_sdk.cpu.llm_message import LLMMessage, AssistantMessage
 from eidolon_sdk.impl.tot_controller.checker import ToTChecker
 from eidolon_sdk.impl.tot_controller.controller import ToTController
@@ -15,25 +17,30 @@ from eidolon_sdk.impl.tot_controller.thought import Thought
 from eidolon_sdk.impl.tot_controller.thought_generators import BaseThoughtGenerationStrategy, ProposePromptStrategy
 from eidolon_sdk.reference_model import Specable, Reference
 from eidolon_sdk.util.class_utils import fqn
+from eidolon_sdk.util.schema_to_model import schema_to_model
 
 
 class ToTAgentConfig(BaseModel):
     num_iterations: int = Field(10, description="The maximum number of iterations to run the tree of thoughts algorithm.")
     num_children: int = Field(10, description="The maximum number of children to generate for each node in the tree of thoughts algorithm.")
+    question_prompt: str = Field(description="The prompt to use when asking the user for a question.")
+    question_json_schema: Dict[str, Any] = Field(description="The json schema for the question input model."),
     thought_generator: Reference[BaseThoughtGenerationStrategy] = Field(
         default=Reference(implementation=fqn(ProposePromptStrategy)),
         description="The thought generator to use."
     )
     checker: Reference[ToTChecker] = Field(default=Reference(implementation=fqn(ToTChecker)), description="The checker to use.")
     fallback: Literal["ERROR", "LLM"] = "ERROR"
+    output_format: Dict[str, Any] = Field(default=None, description="The requested output format of the INIT endpoint.")
 
 
 class TotResponse(BaseModel):
-    answer: str
+    answer: Any
     thoughts: List[str]
 
 
 class TreeOfThoughtsAgent(Agent, Specable[ToTAgentConfig]):
+    spec: ToTAgentConfig
     logger = logging.getLogger("eidolon")
     thought_generator: BaseThoughtGenerationStrategy
     tot_memory: ToTDFSMemory
@@ -56,8 +63,22 @@ class TreeOfThoughtsAgent(Agent, Specable[ToTAgentConfig]):
         text = indent(f"Thought: {thought.text}\n", prefix="    " * level)
         self.logger.info(text)
 
+    def get_input_model(self, action: str):
+        if action == 'INIT':
+            return schema_to_model(self.spec.question_json_schema, "InitialQuestionInputModel")
+        else:
+            return super().get_input_model(action)
+
+    def get_response_model(self, action: str):
+        if action == 'INIT':
+            schema = TotResponse.model_json_schema()
+            schema['properties']['answer'] = self.spec.output_format or dict(type='string')
+            return schema_to_model(schema, "InitialQuestionOutputModel")
+        else:
+            return super().get_response_model(action)
+
     @initializer
-    async def execute_llm(self, question: str, output_format: Dict[str, Any] = None) -> TotResponse:
+    async def execute_llm(self, **kwargs) -> TotResponse:
         """
         Answers a question using the tree of thoughts algorithm. This is computationally expensive, but will provide
         better results than standard llm calls for some problems. Specializes in questions which need to make initial
@@ -66,10 +87,11 @@ class TreeOfThoughtsAgent(Agent, Specable[ToTAgentConfig]):
         """
 
         # override to run the tree of thoughts algorithm in a separate thread
-        context = self.get_context()
-        new_context = context.call_context.derive_call_context()
+        context = CallContext(process_id=self.get_context().process_id, thread_id=None)
+        new_context = context.derive_call_context()
         thoughts_path: List[str] = []
         level = 0
+        question = Environment(undefined=StrictUndefined).from_string(self.spec.question_prompt).render(**kwargs)
 
         async def exec_request(_messages: List[LLMMessage], _output_format: Dict[str, Any]) -> AssistantMessage:
             # todo, this should perhaps use schedule request interface
@@ -90,8 +112,8 @@ class TreeOfThoughtsAgent(Agent, Specable[ToTAgentConfig]):
                     self.logger.warning(f"Problem not solved after after {self.spec.num_iterations} iterations, but falling back to LLM anyway.")
                 # go back to llm now with the tree of thoughts and the requested output format
                 conversation = [UserTextCPUMessage(prompt=question), UserTextCPUMessage(prompt="THOUGHTS\n\n" + ("\n".join(thoughts_path)))]
-                resp = await self.cpu_request(conversation, output_format)
-                return TotResponse(answer=resp.content, thoughts=thoughts_path)
+                resp = await self.cpu_request(conversation, self.spec.output_format)
+                return TotResponse(answer=resp, thoughts=thoughts_path)
             thoughts_path = self.tot_controller.thoughts(self.tot_memory)
 
         if self.spec.fallback == "ERROR":
