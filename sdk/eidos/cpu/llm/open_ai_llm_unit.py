541b2d326e192ab0b1442eeddd0db58b1e4e40d8
dbrewster <dbrewster@paxata.com>
2023-12-07 14:51:34 -0500

    Added an image autonomous agent.

--- sdk/eidos/cpu/llm/open_ai_llm_unit.py
+++ sdk/eidos/cpu/llm/open_ai_llm_unit.py
@@ -1,17 +1,66 @@
+import base64
 import json
 import logging
-from typing import List
+from typing import List, Optional
 
 from openai import AsyncOpenAI
 from openai.types.chat import ChatCompletionToolParam, ChatCompletionMessageToolCall
 from openai.types.chat.completion_create_params import ResponseFormat
 from pydantic import Field
 
+from eidos.agent_os import AgentOS
 from eidos.cpu.call_context import CallContext
 from eidos.cpu.llm_message import LLMMessage, AssistantMessage, ToolCall, ToolResponseMessage, UserMessage, \
     SystemMessage
 from eidos.cpu.llm_unit import LLMUnit, LLMUnitConfig, LLMCallFunction
 from eidos.system.reference_model import Specable
+
+from PIL import Image
+from io import BytesIO
+
+
+def scale_dimensions(width, height, max_size=2048, min_size=768):
+    # Check if the dimensions are less than or equal to max_size.
+    # If so, adjust the dimensions according to the max_size.
+    if width > max_size or height > max_size:
+        # Calculate the scaling ratio
+        scale_ratio = max_size / max(width, height)
+
+        # Calculate the new dimensions while keeping aspect ratio
+        width = int(width * scale_ratio)
+        height = int(height * scale_ratio)
+
+    # Check if the minimum dimension is still greater than the min_size.
+    # If so, adjust the dimensions according to the min_size.
+    if min(width, height) > min_size:
+        # Calculate the scaling ratio
+        scale_ratio = min_size / min(width, height)
+
+        # Calculate the new dimensions
+        width = int(width * scale_ratio)
+        height = int(height * scale_ratio)
+
+    return width, height
+
+
+def scale_image(image_bytes):
+    max_size = 768
+    # Load the image from bytes
+    image = Image.open(BytesIO(image_bytes))
+
+    # Get the dimensions of the image
+    width, height = image.size
+
+    print(f"Original image size: {width}x{height}")
+    new_width, new_height = scale_dimensions(width, height)
+    print(f"New image size: {new_width}x{new_height}")
+
+    # Resize and return the image
+    scaled_image = image.resize((new_width, new_height))
+    output = BytesIO()
+    scaled_image.save(output, format='PNG')
+    return output.getvalue()
+
 
 logger = logging.getLogger("eidolon")
 
@@ -25,7 +74,27 @@ def convert_to_openai(message: LLMMessage):
     elif isinstance(message, UserMessage):
         content = message.content
         if not isinstance(content, str):
-            content = [part.model_dump() for part in content]
+            content = []
+            for part in message.content:
+                if part.type == "text":
+                    content.append({
+                        "type": "text",
+                        "text": part.text
+                    })
+                else:
+                    # retrieve the image from the file system
+                    data = AgentOS.file_memory.read_file(part.image_url)
+                    # scale the image such that the max size of the shortest size is at most 768px
+                    data = scale_image(data)
+                    # base64 encode the data
+                    base64_image = base64.b64encode(data).decode('utf-8')
+                    content.append({
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                    })
+
         return {
             "role": "user",
             "content": content
@@ -59,6 +128,8 @@ def convert_to_openai(message: LLMMessage):
 class OpenAiGPTSpec(LLMUnitConfig):
     model: str = Field(default="gpt-4-1106-preview", description="The model to use for the LLM.")
     temperature: float = 0.3
+    force_json: bool = True
+    max_tokens: Optional[int] = None
 
 
 class OpenAIGPT(LLMUnit, Specable[OpenAiGPTSpec]):
@@ -76,13 +147,16 @@ class OpenAIGPT(LLMUnit, Specable[OpenAiGPTSpec]):
             self.llm = AsyncOpenAI()
         messages = [convert_to_openai(message) for message in inMessages]
 
+        force_json_msg = f"Your response MUST be valid JSON satisfying the following schema:\n{json.dumps(output_format)}"
+        if not self.spec.force_json:
+            force_json_msg += "\nThe response will be wrapped in a json section json```{...}```"
         # add response rules to original system message for this call only
         if messages[0]['role'] == 'system':
-            messages[0]['content'] += f"\n\nYour response MUST be valid JSON satisfying the following schema:\n{json.dumps(output_format)}"
+            messages[0]['content'] += f"\n\n{force_json_msg}"
         else:
             messages.insert(0, {
                 "role": "system",
-                "content": f"Your response MUST be valid JSON satisfying the following schema:\n{json.dumps(output_format)}"
+                "content": force_json_msg
             })
 
         logger.debug(messages)
@@ -100,11 +174,16 @@ class OpenAIGPT(LLMUnit, Specable[OpenAiGPTSpec]):
         request = {
             "messages": messages,
             "model": self.model,
-            "temperature": self.temperature,
-            "response_format": ResponseFormat(type="json_object")
+            "temperature": self.temperature
         }
+        if self.spec.force_json:
+            request["response_format"] = ResponseFormat(type="json_object")
+
         if len(tools) > 0:
             request["tools"] = tools
+
+        if self.spec.max_tokens:
+            request["max_tokens"] = self.spec.max_tokens
 
         logger.info("executing open ai llm request", extra=request)
         try:
@@ -117,9 +196,16 @@ class OpenAIGPT(LLMUnit, Specable[OpenAiGPTSpec]):
         logger.info(f"open ai llm response", extra=dict(content=message.content, tool_calls=message.tool_calls))
 
         tool_response = [_convert_tool_call(tool) for tool in message.tool_calls or []]
+        if not self.spec.force_json:
+            # message format looks like json```{...}```, parse content and pull out the json
+            message_text = message.content[message.content.find("{"):message.content.rfind("}") + 1]
+        else:
+            message_text = message.content
+
         try:
-            content = json.loads(message.content) if message.content else {}
+            content = json.loads(message_text) if message_text else {}
         except json.JSONDecodeError as e:
+            print(message_text)
             raise RuntimeError("Error decoding response content") from e
         return AssistantMessage(content=content, tool_calls=tool_response)
 
