ab5da8d44eb4bac04c13d1cf01acc54d8d1de9d8
Luke Lalor <lukehlalor@gmail.com>
2023-12-09 16:22:13 -0800

    add some docs

--- sdk/eidos/system/reference_model.py
+++ sdk/eidos/system/reference_model.py
@@ -26,11 +26,11 @@ B = TypeVar('B')
 D = TypeVar('D')
 
 
-class Reference(BaseModel, Generic[B, D]):
-    bound: Type[B] = None
-    default: Type[D] = None
-    implementation: str = None
-    spec: dict = None
+class _ReferenceAlias(BaseModel, Generic[B, D]):
+    bound: Type[B]
+    default: Type[D]
+    implementation: str
+    spec: dict
 
     def __class_getitem__(cls, params):
         if not isinstance(params, tuple):
@@ -64,10 +64,10 @@ class Reference(BaseModel, Generic[B, D]):
         if not self.implementation:
             raise ValueError(f'Unable to determine implementation for "{self}"')
 
-        self.build_reference_spec()
+        self._build_reference_spec()
         return self
 
-    def build_reference_spec(self):
+    def _build_reference_spec(self):
         reference_class = self._get_reference_class()
         if issubclass(reference_class, Specable):
             bases = getattr(reference_class, '__orig_bases__', [])
@@ -81,16 +81,51 @@ class Reference(BaseModel, Generic[B, D]):
         else:
             return self.spec
 
-    def instantiate(self, *args, **kwargs):
-        spec = self.build_reference_spec()
-        if spec is not None:
-            kwargs['spec'] = spec
-        return self._get_reference_class()(*args, **kwargs)
-
     def _get_reference_class(self):
         return for_name(self.implementation, self.bound or object)
 
 
+class Reference(_ReferenceAlias[B, D], Generic[B, D]):
+    """
+    A subclass of _ReferenceAlias that is used to create references to other classes. It is designed to be used with
+    two type variables, B (type Bound) and D (default type). They are BaseModel attributes, but can be largely ignored.
+
+    Attributes:
+        implementation (str): Represents the fully qualified name of the class that the reference points to. It is
+            optional and will default to the name of the default class or bound (from generics).
+        spec (dict): Holds any additional specifications for the reference. It is used to instantiate the reference.
+    """
+
+    bound: Type[B] = None
+    default: Type[D] = None
+    implementation: str = None
+    spec: dict = None
+
+    def instantiate(self, *args, **kwargs):
+        spec = self._build_reference_spec()
+        if spec is not None:
+            kwargs['spec'] = spec
+        return self._get_reference_class()(*args, **kwargs)
+
+
 class AnnotatedReference(Reference):
+    """
+    Helper class to manage References with defaults so that they do not need to be declared.
+    Wraps the returned type in Annotated so that the default can be defined in the type annotation.
+
+    This is important since defaults should be partially defined so that partial references can be defined.
+    Ie, providing only the spec and accepting the default implementation or defining the implementation only, but
+    accepting that implementation's default spec.
+
+    One may be tempted to leave the Default off of the type annotation since it is declared later, but this would
+    prevent users from overriding just the spec
+
+    Example:
+        class MySpec(BaseModel):
+            ref1: AnnotatedReference[MyBound, MyDefaultImpl)] = Field(description="My description")
+
+    Note:
+        The description can still be added via a Field annotation without affecting default behavior
+    """
     def __class_getitem__(cls, params) -> Type[Reference]:
         return Annotated[Reference[params], Field(default=Reference[params]())]
