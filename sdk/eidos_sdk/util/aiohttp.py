6cf0536bd5cf261eb86898ff4ef2fa0a2e3291be
Luke Lalor <lukehlalor@gmail.com>
2024-01-09 13:59:59 -0800

    rename ClientSession to avoid confusion

--- sdk/eidos_sdk/util/aiohttp.py
+++ sdk/eidos_sdk/util/aiohttp.py
@@ -3,7 +3,7 @@ from aiohttp import ClientSession as _ClientSession
 from eidos_sdk.system.request_context import RequestContext
 
 
-class ClientSession(_ClientSession):
+class ContextualClientSession(_ClientSession):
     def __init__(self, *args, **kwargs):
         if 'headers' not in kwargs:
             kwargs['headers'] = {}
