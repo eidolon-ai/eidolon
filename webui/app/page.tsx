28af1d983a71881fecb5770a61b08aad3715398e
Dave Brewster <davebrewster@me.com>
2024-02-21 10:11:12 -0500

    Add background screen.  (#93)
    Fix bug#90

--- webui/app/page.tsx
+++ webui/app/page.tsx
@@ -1,11 +1,17 @@
 import {getChats} from "@/app/api/chat/route";
 import {redirect} from "next/navigation";
+import {getServerSession} from "next-auth";
 
 export const runtime = 'nodejs'
 
 export default async function IndexPage() {
-  const chats = await getChats().then(c => c.reverse())
-  if (chats.length != 0) {
-    redirect(`/chat/${chats[0].id}`)
+  const session = await getServerSession()
+
+  if (!session?.user) {
+    redirect(`/sign-in?next=/`)
   }
+
+  return (
+    <img src="/background.png" alt="background" style={{height:"100%", opacity:.8}} />
+  )
 }
