6c7bf380dbe094642204a4ea06006785fd8c26fd
dbrewster <davebrewster@me.com>
2024-02-22 19:20:01 -0500

    Remove program from system Starting with example tests

--- examples/tests/test_hello_world.py
+++ examples/tests/test_hello_world.py
@@ -1,6 +1,8 @@
 import pytest
 from jsonref import requests
 
+from conftest import get_process_id
+
 
 @pytest.fixture(scope="module", autouse=True)
 def http_server(eidolon_server, eidolon_examples):
@@ -11,12 +13,13 @@ def http_server(eidolon_server, eidolon_examples):
 def test_server_is_running(server_loc):
     response = requests.get(f"{server_loc}/openapi.json")
     assert response.status_code == 200
-    assert "/agents/ExampleGeneric/programs/question" in response.json()["paths"]
+    assert "/agents/ExampleGeneric/processes/{process_id}/actions/question" in response.json()["paths"]
 
 
 def test_can_hit_generic_agent(server_loc):
+    process_id = get_process_id(server_loc, "ExampleGeneric")
     response = requests.post(
-        f"{server_loc}/agents/ExampleGeneric/programs/question",
+        f"{server_loc}/agents/ExampleGeneric/processes/{process_id}/actions/question",
         json=dict(instruction="Hi! What is the capital of France?"),
     )
     assert response.status_code == 200
@@ -24,12 +27,12 @@ def test_can_hit_generic_agent(server_loc):
 
 
 def test_tool_calls(server_loc):
+    process_id = get_process_id(server_loc, "ExampleGeneric")
     # prep with some information to use on the subsequent response to demonstrate is handled well
     response = requests.post(
-        f"{server_loc}/agents/ExampleGeneric/programs/question", json=dict(instruction="Hi! My name is Luke.")
+        f"{server_loc}/agents/ExampleGeneric/processes/{process_id}/actions/question", json=dict(instruction="Hi! My name is Luke.")
     )
     response.raise_for_status()
-    process_id = response.json()["process_id"]
     response2 = requests.post(
         f"{server_loc}/agents/ExampleGeneric/processes/{process_id}/actions/respond",
         json=dict(statement="Please use the HelloWorld tool with my name."),
