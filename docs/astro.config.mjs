cabd05f1dd8c2932963fb6d3762b546ddc1cc0f8
dbrewster <davebrewster@me.com>
2024-02-26 11:39:30 -0800

    Added analytics

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -13,6 +13,24 @@ export default defineConfig({
 //                 Hero: './src/components/NewHero.astro',
 //             },
             favicon: '/favicon.ico',
+            head: [
+                {
+                    tag: 'script',
+                    attrs: {
+                        // Tweaks to the script URL or attributes can be made here.
+                        src: 'https://www.googletagmanager.com/gtag/js?id=G-ES73QFGWZ1',
+                        async: true,
+                    },
+                },
+                {
+                    tag: 'script',
+                    content: '  window.dataLayer = window.dataLayer || [];\n' +
+                        '  function gtag(){dataLayer.push(arguments);}\n' +
+                        '  gtag(\'js\', new Date());\n' +
+                        '\n' +
+                        '  gtag(\'config\', \'G-ES73QFGWZ1\');\n'
+                },
+            ],
             social: {
                 github: 'https://github.com/eidolon-ai/eidolon',
                 youtube: 'https://www.youtube.com/channel/UCARP0MIGLlq9BArL6HG6eUg',
@@ -20,27 +38,32 @@ export default defineConfig({
                 linkedin: 'https://www.linkedin.com/company/august-data/',
             },
             sidebar: [
-				{
-					label: 'Getting Started',
-					items: [
-						{ label: 'Quickstart', link: '/getting_started/quickstart/' },
-						{ label: 'Tutorials', link: '/getting_started/tutorials/' },
-						{ label: 'References', link: '/getting_started/references/' },
-					],
+                {
+                    label: 'Getting Started',
+                    items: [
+                        {label: 'Quickstart', link: '/getting_started/quickstart/'},
+                        {label: 'Tutorials', link: '/getting_started/tutorials/'},
+                        {label: 'References', link: '/getting_started/references/'},
+                    ],
 // 					autogenerate: { directory: 'getting_started'},
                 },
                 {
                     label: 'Architecture',
                     items: [
-                        { label: 'Introduction', link: '/architecture/introduction/' },
-                        { label: 'Fundamentals', link: '/architecture/fundamentals/' },
-                        { label: 'Agent Program', link: '/architecture/agent_program/' },
-						{ label: 'Agent CPU', link: '/architecture/agent_cpu/' },
-						{ label: 'AgentOS', link: '/architecture/agent_os/' },
-						{ label: 'Conclusion', link: '/architecture/conclusion/' },
-					],
-				},
+                        {label: 'Introduction', link: '/architecture/introduction/'},
+                        {label: 'Fundamentals', link: '/architecture/fundamentals/'},
+                        {label: 'Agent Program', link: '/architecture/agent_program/'},
+                        {label: 'Agent CPU', link: '/architecture/agent_cpu/'},
+                        {label: 'AgentOS', link: '/architecture/agent_os/'},
+                        {label: 'Conclusion', link: '/architecture/conclusion/'},
+                    ],
+                },
             ],
+        }),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
         }),
     ],
 });
