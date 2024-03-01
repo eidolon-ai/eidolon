4e4558a424974760e02de1b2500c61ba25a9ffa6
dbrewster <davebrewster@me.com>
2024-03-01 15:31:05 -0800

    Made stripe work

--- webui/app/api/checkout_sessions/route.ts
+++ webui/app/api/checkout_sessions/route.ts
@@ -1,34 +1,34 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import Stripe from 'stripe';
+import {redirect} from "next/navigation";
+import { NextResponse } from 'next/server';
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const baseURL = process.env.NEXTAUTH_URL;
 
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        try {
-            // Create Checkout Sessions from body params.
-            console.log("stripe_before")
-            const session = await stripe.checkout.sessions.create({
-                line_items: [{
-                    // Provide the exact Price ID of the product you want to sell
-                    price: 'price_1OoGyiGBK4zTc9Yyi2Nk52on',
-                    quantity: 1,
-                }],
-                mode: 'payment',
-                success_url: `http://localhost:3000/profile/?success=true`,
-                cancel_url: `http://localhost:3000/profile/?canceled=true`,
-                automatic_tax: { enabled: true },
-            });
-            console.log("stripe_after")
-            // Redirect to the session URL
-            console.log(res)
-            return res.redirect(303, session.url);
-        } catch (err) {
-            // Make sure to return a response in case of error
-            console.error('Error creating Stripe Session:', err);
-            return new Response('Failed to create Stripe Session', {status: 500})
-        }
-    } else {
-        // Handle any non-POST requests
-        return res.setHeader('Allow', ['POST']).status(405).end(`Method ${req.method} Not Allowed`);
-    }
+  try {
+    // Create Checkout Sessions from body params.
+    console.log("stripe_before")
+    const session = await stripe.checkout.sessions.create({
+      line_items: [{
+        // Provide the exact Price ID of the product you want to sell
+        price: 'price_1OoGyiGBK4zTc9Yyi2Nk52on',
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: `${baseURL}/profile/?success=true`,
+      cancel_url: `${baseURL}/profile/?canceled=true`,
+      automatic_tax: {enabled: true},
+    });
+    console.log("stripe_after")
+    // Redirect to the session URL
+    console.log(res)
+    console.log("*****session", session.url)
+    return NextResponse.redirect(session.url!, 302);
+  } catch (err) {
+    // Make sure to return a response in case of error
+    console.error('Error creating Stripe Session:', err);
+    return new Response('Failed to create Stripe Session', {status: 500})
+  }
 }
--- webui/app/profile/page.tsx
+++ webui/app/profile/page.tsx
@@ -1,12 +1,9 @@
 'use client'
 import React from 'react';
-import { loadStripe } from '@stripe/stripe-js';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ''
-);
+
 export default function PreviewPage() {
   React.useEffect(() => {
     // Check to see if this is a redirect back from Checkout
