4c65d02c707f2a111983d1a749a8dd5bf91f7343
dbrewster <davebrewster@me.com>
2024-03-08 21:13:53 -0500

    Added research tool

--- sdk/eidolon_ai_sdk/builtins/logic_units/web_search.py
+++ sdk/eidolon_ai_sdk/builtins/logic_units/web_search.py
@@ -1,16 +1,17 @@
+import asyncio
 import os
 from contextlib import asynccontextmanager
 from typing import List, Optional
 
+import dotenv
 # from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from httpx import AsyncClient, Timeout
-from jinja2 import Environment, StrictUndefined
 from pydantic import BaseModel, Field
 
+from eidolon_ai_client.util.logger import logger
 from eidolon_ai_sdk.cpu.logic_unit import LogicUnit, llm_function
 from eidolon_ai_sdk.system.reference_model import Specable
-from eidolon_ai_client.util.logger import logger
 
 
 class SearchResult(BaseModel):
@@ -24,6 +25,8 @@ class WebSearchConfig(BaseModel):
     cse_id: str = Field(None, desctiption="Google Custom Search Engine Id.")
     cse_token: str = Field(None, desctiption="Google Project dev token, must have search permissions.")
     summarizer: Optional[str] = "BeautifulSoup"
+    siteSearch: Optional[str] = Field(None, description="Restricts results to URLs from a specified site.")
+    includeSiteSearchSites: Optional[bool] = Field(True, description="Controls whether to include or exclude results from the site specified in siteSearch.")
 
 
 class WebSearch(LogicUnit, Specable[WebSearchConfig]):
@@ -32,14 +35,13 @@ class WebSearch(LogicUnit, Specable[WebSearchConfig]):
         Specable.__init__(self, **kwargs)
         self.spec.cse_id = self.spec.cse_id or os.environ["CSE_ID"]
         self.spec.cse_token = self.spec.cse_token or os.environ["CSE_TOKEN"]
-        self.jinja_env = Environment(undefined=StrictUndefined)
         if not self.spec.cse_id or not self.spec.cse_token:
             raise ValueError("missing required cse_id or cse_token")
 
     @asynccontextmanager
     async def _get(self, **kwargs):
         async with AsyncClient(timeout=Timeout(5.0, read=600.0)) as client:
-            yield await client.get(**kwargs)
+            yield await client.get(**kwargs, follow_redirects=True)
 
     @llm_function()
     async def go_to_url(self, url: str) -> str:
@@ -61,10 +63,10 @@ class WebSearch(LogicUnit, Specable[WebSearchConfig]):
 
     @llm_function()
     async def search(
-        self,
-        term: str,
-        num_results: int = 10,
-        lang: str = "en",
+            self,
+            term: str,
+            num_results: int = 10,
+            lang: str = "en",
     ) -> List[SearchResult]:
         """
         Search google and get the results. Cannot return more than 100 results
@@ -80,21 +82,41 @@ class WebSearch(LogicUnit, Specable[WebSearchConfig]):
             raise ValueError("Cannot return more than 100 results")
         escaped_term = term.replace(" ", "+")
         resp = await self._req(escaped_term, num_results, lang)
-        if not resp["items"]:
-            raise RuntimeError("Error retrieving results")
-        for item in resp["items"]:
+        items = []
+        if "items" in resp:
+            items = resp["items"]
+        for item in items:
             yield SearchResult(url=item["link"], title=item["title"], description=item.get("snippet"))
 
     async def _req(self, term, results, lang):
+        params = {
+            "q": term,
+            "num": results,  # Prevents multiple requests
+            "hl": lang,
+            "cx": self.spec.cse_id,
+            "key": self.spec.cse_token,
+        }
+        if self.spec.siteSearch:
+            params["siteSearch"] = self.spec.siteSearch
+            params["siteSearchFilter"] = "i" if self.spec.includeSiteSearchSites else "e"
+
         async with self._get(
-            url="https://customsearch.googleapis.com/customsearch/v1",
-            params={
-                "q": term,
-                "num": results,  # Prevents multiple requests
-                "hl": lang,
-                "cx": self.spec.cse_id,
-                "key": self.spec.cse_token,
-            },
-        ) as resp:
+                url="https://customsearch.googleapis.com/customsearch/v1",
+                params=params) as resp:
             resp.raise_for_status()
             return resp.json()
+
+
+async def main():
+    dotenv.load_dotenv()
+    search = WebSearch(spec=WebSearchConfig(siteSearch="linkedin.com/jobs"), processing_unit_locator=None)
+    results = await search.search("enterprise LLM agent prompt")
+    for result in results:
+        print(result.title)
+        print(result.url)
+        print(result.description)
+        print()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
