e778393f14f690984404ba056f3ea411fcbe6fa4
dbrewster <davebrewster@me.com>
2024-03-19 12:30:39 -0400

    Removed old app

--- webui/apps/eidolon-ui/app/api/chat/route.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import {revalidateTag} from 'next/cache'
-
-import {type Chat, ProcessState} from '@/lib/types'
-import {getServerSession} from "next-auth";
-import {DateTime, Interval} from "luxon";
-import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
-
