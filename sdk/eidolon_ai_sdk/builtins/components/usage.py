3a9fb8c7859ca3d90a2048897cf90335914470cf
Luke Lalor <lukehlalor@gmail.com>
2024-03-21 01:18:00 -0700

    one resource to rule them all

--- sdk/eidolon_ai_sdk/builtins/components/usage.py
+++ sdk/eidolon_ai_sdk/builtins/components/usage.py
@@ -5,27 +5,43 @@ from re import Pattern
 import math
 from openai import BaseModel
 from opentelemetry import trace
-from opentelemetry.sdk.trace import SpanProcessor, ReadableSpan
+from opentelemetry.sdk.trace import ReadableSpan
+from opentelemetry.sdk.trace import TracerProvider, SpanProcessor
 from starlette.responses import JSONResponse
 
 from eidolon_ai_client.util.logger import logger
 from eidolon_ai_client.util.request_context import RequestContext
 from eidolon_ai_sdk.security.user import User
 from eidolon_ai_sdk.system.dynamic_middleware import Middleware
-from eidolon_ai_sdk.system.reference_model import AnnotatedReference, Specable
+from eidolon_ai_sdk.system.reference_model import AnnotatedReference
 from usage_client.client import UsageClient, UsageLimitExceeded
 from usage_client.models import UsageDelta
 
-action_pattern: Pattern[str] = re.compile(r"^POST /processes/([^/]+)/agent/([^/]+)/actions/([^\s/]+)$")
+default_cost_pattern: Pattern[str] = re.compile(r"^POST /processes/([^/]+)/agent/([^/]+)/actions/([^\s/]+)$")
+default_refund_pattern: Pattern[str] = re.compile(r"tool\scalls")
 
 
-class UsageMiddleware(Middleware, BaseModel):
-    path_regex: str = None
+class UsageMiddleware(BaseModel, Middleware):
+    cost_regex: str = None
+    refund_regex: str = None
     usage_client: AnnotatedReference[UsageClient]
 
+    def __init__(self):
+        super().__init__()
+        UsageSpanProcessor.register(
+            client=self.usage_client.instantiate(),
+            cost_pattern=self.cost_pattern(),
+            refund_pattern=self.refund_pattern()
+        )
+
+    def cost_pattern(self):
+        return re.compile(self.cost_regex) if self.cost_regex else default_cost_pattern
+
+    def refund_pattern(self):
+        return re.compile(self.refund_regex) if self.refund_regex else default_refund_pattern
+
     async def dispatch(self, request, call_next):
-        search_pattern = re.compile(self.path_regex) if self.path_regex else action_pattern
-        if search_pattern.search(f"{request.method} {request.url.path}"):
+        if self.cost_pattern().search(f"{request.method} {request.url.path}"):
             trace.get_current_span().set_attribute("usage_subject", User.get_current().id)
             client: UsageClient = self.usage_client.instantiate()
             subject = User.get_current().id
@@ -48,33 +64,32 @@ class UsageMiddleware(Middleware, BaseModel):
 async def _billing(client, subject, amount):
     try:
         await client.record_transaction(subject, UsageDelta(used_delta=amount))
-        logger.info(f"Billed Usage: {amount}")
+        logger.info(f"Recorded Usage: {amount}")
     except Exception:
         logger.exception("Error recording usage")
         raise
 
 
-class UsageSpanProcessorConfig(BaseModel):
-    usage_client: AnnotatedReference[UsageClient]
-    wrapped: AnnotatedReference[SpanProcessor]
-    refund_regex: str = "tool\scalls"
+class UsageSpanProcessor(SpanProcessor):
+    _added = False
+    usage_client: UsageClient
+    cost_pattern: Pattern[str]
+    refund_pattern: Pattern[str]
 
+    def __init__(self, client, cost_pattern, refund_pattern):
+        self.usage_client = client
+        self.cost_pattern = cost_pattern
+        self.refund_pattern = refund_pattern
 
-class UsageSpanProcessor(SpanProcessor, Specable[UsageSpanProcessorConfig]):
-    wrapped: SpanProcessor
-    cost_pattern = Pattern[str]
-    refund_pattern = Pattern[str]
-
-    def __init__(self, *args, spec, **kwargs):
-        SpanProcessor.__init__(self)
-        Specable.__init__(self, spec)
-        self.wrapped = spec.wrapped.instantiate(*args, **kwargs)
-        self.usage_client = spec.usage_client.instantiate()
-        self.cost_pattern = action_pattern
-        self.refund_pattern = re.compile(spec.refund_regex)
+    @staticmethod
+    def register(**kwargs):
+        if not UsageSpanProcessor._added:
+            UsageSpanProcessor._added = True
+            trace_provider: TracerProvider = trace.get_tracer_provider()
+            trace_provider.add_span_processor(UsageSpanProcessor(**kwargs))
 
     def on_start(self, *args, **kwargs):
-        return self.wrapped.on_start(*args, **kwargs)
+        pass
 
     def on_end(self, span: ReadableSpan):
         try:
@@ -91,7 +106,6 @@ class UsageSpanProcessor(SpanProcessor, Specable[UsageSpanProcessorConfig]):
                 RequestContext.set("eidolon_refund", refunded + duration)
         except Exception:
             logger.exception("Error billing usage")
-        return self.wrapped.on_end(span)
 
     def shutdown(self):
-        return self.wrapped.shutdown()
+        pass
