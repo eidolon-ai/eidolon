f2777232f1110d24933e6504ac095c700bd9c640
dbrewster <davebrewster@me.com>
2024-04-22 13:17:53 -0400

    Added tests for processes

--- webui/packages/eidolon-components/src/lib/util.ts
+++ webui/packages/eidolon-components/src/lib/util.ts
@@ -1,4 +1,4 @@
-import {OperationInfo} from "@eidolon/client";
+import {HttpException, OperationInfo} from "@eidolon/client";
 
 export interface CopilotParams {
   "agent": string,
@@ -12,5 +12,27 @@ export interface CopilotParams {
   "speechAgent": string | undefined,
   "speechOperation": string | undefined
 }
+
 export interface DevParams {
+}
+
+
+export function processResponse(promise: Promise<any>) {
+  return convertException(promise.then(Response.json))
+}
+
+export function convertException(promise: Promise<any>) {
+  return promise.catch((e) => {
+    if (e instanceof HttpException) {
+      return new Response(e.statusText, {status: e.status, statusText: e.statusText})
+    } else if (e instanceof Error) {
+      return new Response(e.message, {status: 500})
+    } else {
+      if (e?.cause?.code === 'ECONNREFUSED') {
+        return new Response('Connection refused', {status: 404})
+      }
+
+      return new Response('Unknown error', {status: 500})
+    }
+  })
 }
---
+++ webui/packages/eidolon-components/test/server/process-server-handler.test.ts
@@ -0,0 +1,65 @@
+import {createMocks} from 'node-mocks-http'
+import {describe, expect, test} from "@jest/globals"
+import {ProcessesHandler} from "../../src/server/processes-server-handler"
+import autoSetupPolly from "../polly_helper"
+
+const processesHandler = new ProcessesHandler(async () => '')
+
+describe('server-handler', () => {
+  autoSetupPolly()
+
+  describe('ProcessesHandler', () => {
+    test('GET returns process list', async () => {
+      // const process = await processesHandler.createProcess('http://localhost:8080', 'conversational_agent', 'title')
+      const {req} = createMocks({
+        method: 'GET',
+        url: 'http://localhost/processes?machineURL=http://localhost:8080'
+      })
+      // @ts-ignore
+      const response = await processesHandler.GET(req)
+      expect(response.status).toBe(200)
+      const json = await response.json()
+      expect(json).toEqual(
+          {
+            "next": "http://localhost:8080/processes?skip=0&limit=100/processes/?limit=100&skip=100",
+            "processes": [
+              {
+                "agent": "conversational_agent",
+                "available_actions": [
+                  "converse",
+                  "generate_title"
+                ],
+                "created": "2024-04-22T11:15:34.241671",
+                "machine": "http://localhost:8080",
+                "parent_process_id": null,
+                "process_id": "66267f160c3a347cff5dc9db",
+                "state": "initialized",
+                "title": "title",
+                "updated": "2024-04-22T11:15:34.241671"
+              }
+            ],
+            "total": 1
+          }
+      )
+    })
+    test('GET returns 422 if machineURL is missing', async () => {
+      const {req} = createMocks({
+        method: 'GET',
+        url: 'http://localhost/processes'
+      })
+      // @ts-ignore
+      const response = await processesHandler.GET(req)
+      expect(response.status).toBe(422)
+    })
+
+    test('GET returns 404 if server is down', async () => {
+      const {req} = createMocks({
+        method: 'GET',
+        url: 'http://localhost/processes?machineURL=http://localhost:8080'
+      })
+      // @ts-ignore
+      const response = await processesHandler.GET(req)
+      expect(response.status).toBe(404)
+    })
+  })
+})
