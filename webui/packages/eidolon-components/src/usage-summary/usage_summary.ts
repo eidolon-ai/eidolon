f47baf81ad2210de1da361497b7a8cfe4ffa6ca6
Luke Lalor <lukehlalor@gmail.com>
2024-04-29 12:35:03 -0700

    typescript evidently flattens promises

--- webui/packages/eidolon-components/src/usage-summary/usage_summary.ts
+++ webui/packages/eidolon-components/src/usage-summary/usage_summary.ts
@@ -2,13 +2,13 @@
 
 import {OpenAPI, UsageService} from "@eidolon/usage-client";
 
-export function usageForSession(sub: string) {
+export async function usageForSession(sub: string) {
   // eslint-disable-next-line no-undef
   const usageServerLoc = process.env.EIDOLON_USAGE_SERVER;
   if (usageServerLoc) {
     OpenAPI.BASE = usageServerLoc
     return UsageService.getUsageSummarySubjectsSubjectIdGet({subjectId: sub})
   } else {
-    return Promise.resolve(null)
+    return null
   }
 }
