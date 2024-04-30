ef5e16739290dd7c6d13d775e3f1b8b2c77847ac
Luke Lalor <lukehlalor@gmail.com>
2024-04-29 21:42:38 -0700

    lint

--- webui/packages/eidolon-components/src/usage-summary/usage_summary.ts
+++ webui/packages/eidolon-components/src/usage-summary/usage_summary.ts
@@ -4,12 +4,13 @@ import {OpenAPI, UsageService, UsageSummary} from "@eidolon/usage-client";
 
 export async function usageForSession(sub: string): Promise<UsageSummary | null> {
   // eslint-disable-next-line no-undef
-  OpenAPI.BASE = process.env.EIDOLON_USAGE_SERVER || "http://localhost:8527"
+  const envUsageLoc = process.env.EIDOLON_USAGE_SERVER;
+  OpenAPI.BASE = envUsageLoc || "http://localhost:8527"
   return UsageService.getUsageSummarySubjectsSubjectIdGet({subjectId: sub}).catch((e: any) => {
-    if (!process.env.EIDOLON_USAGE_SERVER) {
+    if (!envUsageLoc) {
       console.info("Usage server is not available")
     } else if ('cause' in e && e.cause.code === "ECONNREFUSED") {
-      console.error("Usage server is not available:", process.env.EIDOLON_USAGE_SERVER)
+      console.error("Usage server is not available:", envUsageLoc)
     } else {
       console.error(e)
     }
