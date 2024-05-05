d8f4d655d92864849d470e76d11cd9c2af9450f3
Luke Lalor <lukehlalor@gmail.com>
2024-05-04 20:43:36 -0700

    stash

--- webui/apps/docs/astro.config.mjs
+++ webui/apps/docs/astro.config.mjs
@@ -76,7 +76,7 @@ export default defineConfig({
           ],
         },
         {
-          label: 'References',
+          label: 'Features',
           items: [
             {label: 'Introduction', link: '/docs/references/introduction/'},
             {label: 'Setup', link: '/docs/references/setup'},
@@ -85,6 +85,36 @@ export default defineConfig({
             {label: 'Pluggable Resources', link: '/docs/references/pluggable'},
             {label: 'Defining a Machine', link: '/docs/references/defining'},
           ],
+        },
+        {
+          label: 'Components (builtin)',
+          items: [
+            {label: 'Components Overview', link: '/docs/recipes/under-construction/'},
+            {
+              label: 'Agent Templates', collapsed: true, items: [
+                {label: 'Agent Template Overview', link: '/docs/recipes/under-construction/'},
+                {label: 'SimpleAgent', link: '/docs/recipes/under-construction/'},
+                {label: 'RetrieverAgent', link: '/docs/recipes/under-construction/'},
+              ]
+            },
+            {label: 'Agent Processing Unit (APU)', link: '/docs/recipes/under-construction/'},
+            {
+              label: 'LLM Units', collapsed: true, items: [
+                {label: 'LLM Unit Overview', link: '/docs/recipes/under-construction/'},
+                {label: 'OpenAI', link: '/docs/recipes/under-construction/'},
+                {label: 'Mistral', link: '/docs/recipes/under-construction/'},
+                {label: 'Anthropic', link: '/docs/recipes/under-construction/'},
+              ]
+            },
+            {
+              label: 'Logic Unit', collapsed: true, items: [
+                {label: 'Logic Unit Overview', link: '/docs/recipes/under-construction/'},
+                {label: 'Overview', link: '/docs/recipes/under-construction/'},
+                {label: 'Search', link: '/docs/recipes/under-construction/'},
+                {label: 'Browser', link: '/docs/recipes/under-construction/'},
+              ]
+            },
+          ]
         },
         {
           label: 'Architecture',
