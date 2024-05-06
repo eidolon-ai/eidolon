5637146b76e980ae8ddd5770db6b7e4d80fd57fb
dbrewster <davebrewster@me.com>
2024-05-06 08:53:39 -0400

    Added stars and forks to script

---
+++ scripts/scripts/push_github_watches.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from scripts.base_push_github_stats import run_script
+
+
+def extract_data(event):
+    return {
+        "owner_id": event["id"],
+        "owner_login": event["login"],
+        "distinct_id": event["login"],
+        "email": event.get("email") or "",
+        "timestamp": event.get("starred_at") or datetime.now().isoformat(),
+    }
+
+
+def main():
+    raise "Broken -- need a date time field to"
+    run_script("vnd.github+json", "subscribers", "gh_watchers", extract_data, "created", "desc")
+
+
+if __name__ == "__main__":
+    main()
