f0d5a20650996b92247f284ba9faa8a4dea722cf
Luke Lalor <lukehlalor@gmail.com>
2024-05-13 11:28:56 -0700

    stash

--- scripts/scripts/docbuilder.py
+++ scripts/scripts/docbuilder.py
@@ -1,53 +1,61 @@
 import json
 import os
-from typing import Optional, cast
+from pathlib import Path
+from typing import cast, Optional
 
 from eidolon_ai_sdk.builtins.code_builtins import named_builtins
 from eidolon_ai_sdk.cpu.llm_unit import LLMUnit
 from eidolon_ai_sdk.system.reference_model import Specable
 from eidolon_ai_sdk.system.resources.reference_resource import ReferenceResource
-from eidolon_ai_sdk.util.class_utils import for_name
+from eidolon_ai_sdk.util.class_utils import for_name, fqn
 from pydantic import BaseModel
 
 
 class Group(BaseModel):
-    pointer: str
-    base: Optional[type]
-    additional: list[type] = []
+    base: type
+    default: Optional[str] = None
+    components: list[type] = []
 
 
-groups = [
-    Group(pointer="LLMUnit", base=LLMUnit)
-]
+groups = {"LLMUnit": Group(base=LLMUnit)}
 
 
 def main():
     resources: list[ReferenceResource] = named_builtins()
+    impls = {}
     for r in resources:
         key = r.metadata.name
         pointer = r.spec
+        if isinstance(pointer, dict) and len(pointer) == 1 and "implementation" in pointer:
+            pointer = pointer["implementation"]
         if not isinstance(pointer, str):
             raise ValueError("Expected pointer of named builtins to be strings")
-        try:
-            clz = for_name(pointer, object)
-        except ValueError:
-            print(f"skipping {key}")
-            break
-        if isinstance(clz, Specable):
-            clz = clz.get_spec_type()
-        if not isinstance(clz, BaseModel):
-            print(f"skipping {key}; not BaseModel")
-            break
+        if key in groups:
+            groups[key].default = pointer
+        else:
+            try:
+                clz = for_name(pointer, object)
+                for g in groups.values():
+                    if issubclass(clz, g.base):
+                        g.components.append(clz)
+            except ValueError as ve:
+                print(f"skipping {key}", ve)
 
-        json_schema = cast(BaseModel, clz).schema_json
-        write_loc = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) / "dist" / f"{key}.json"
+    for key, group in groups.items():
+        json_schema = cast(BaseModel, clz).model_json_schema()
+        write_loc = Path(os.path.dirname(os.path.dirname(__file__))) / "dist" / "component_schemas" / key / "overview.json"
+        os.makedirs(os.path.dirname(write_loc), exist_ok=True)
 
-        with open(write_loc, 'w') as file:
-            json.dump(json_schema, file)
+        with open(write_loc, "w") as file:
+            json.dump(
+                dict(key=key, contract=fqn(group.base), default=group.default, components=group.components),
+                file,
+                indents=2,
+            )
 
-
-
-
+        for component in group.components:
+            with open(write_loc, 'w') as file:
+                json.dump(json_schema, file, indent=2)
 
 
 if __name__ == "__main__":
