d0e9936b34d04bcc6a6aa85711c20f4d3ba3db76
Jacob Habib <jahgoreds@gmail.com>
2024-05-29 00:17:32 -0500

    screenshots on failure

--- webui/apps/eidolon-ui2/playwright.config.js
+++ webui/apps/eidolon-ui2/playwright.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     use: {
         headless: true,
         baseURL: 'http://localhost:3000',
+        screenshot: 'only-on-failure',
     },
     webServer: {
         command: 'pnpm docker-compose up',
