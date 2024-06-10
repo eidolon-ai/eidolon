91b98c22ddfe76e07ee36233239caf3aa92db5ef
Jacob Habib <jahgoreds@gmail.com>
2024-06-10 00:53:47 -0500

    packaging monorepos

--- webui/packages/eidolon-components/index.ts
+++ webui/packages/eidolon-components/index.ts
@@ -1,19 +1,19 @@
-export {ProcessesHandler, ProcessEventsHandler, ProcessHandler} from './src/server/processes-server-handler'
-export {ProcessList, type ProcessListProps} from './src/process-list/process-list'
-export * from './src/client-api-helpers/process-helper'
-export {EidolonEvents, type EidolonEventProps} from './src/messages/eidolon-events'
-export {AgentProcess} from './src/form-input/agent-process'
-export {ChooseAgentForm} from "./src/form-input/choose-agent-form"
-export {executeServerOperation, getChatEventInUI} from "./src/client-api-helpers/process-event-helper";
-export {getOperations, getAgents, getApps} from "./src/client-api-helpers/machine-helper";
-export {type ElementsAndLookup} from "./src/lib/display-elements";
-export * from './src/hooks/useProcessEvents'
-export {CopilotPanel} from './src/form-input/copilot_panel'
-export {DevPanel} from './src/form-input/DevPanel'
-export {useProcessEvents} from './src/hooks/useProcessEvents'
-export * from "./src/lib/util"
-export {usageForSession} from "./src/usage-summary/usage_summary"
-export {EidolonProvider, useEidolonContext} from "./src/provider/eidolon_provider"
-export {useProcesses, ProcessesProvider} from "./src/hooks/processes_context"
-export {useProcess, ProcessProvider} from "./src/hooks/process_context"
-export {CircularProgressWithContent} from "./src/lib/circular-progress-with-content"
+export { ProcessesHandler, ProcessEventsHandler, ProcessHandler } from './src/server/processes-server-handler.js';
+export { ProcessList, type ProcessListProps } from './src/process-list/process-list.js';
+export * from './src/client-api-helpers/process-helper.js';
+export { EidolonEvents, type EidolonEventProps } from './src/messages/eidolon-events.js';
+export { AgentProcess } from './src/form-input/agent-process.js';
+export { ChooseAgentForm } from "./src/form-input/choose-agent-form.js";
+export { executeServerOperation, getChatEventInUI } from "./src/client-api-helpers/process-event-helper.js";
+export { getOperations, getAgents, getApps } from "./src/client-api-helpers/machine-helper.js";
+export { type ElementsAndLookup } from "./src/lib/display-elements.js";
+export * from './src/hooks/useProcessEvents.js';
+export { CopilotPanel } from './src/form-input/copilot_panel.js';
+export { DevPanel } from './src/form-input/DevPanel.js';
+export { useProcessEvents } from './src/hooks/useProcessEvents.js';
+export * from "./src/lib/util.js";
+export { usageForSession } from "./src/usage-summary/usage_summary.js";
+export { EidolonProvider, useEidolonContext } from "./src/provider/eidolon_provider.js";
+export { useProcesses, ProcessesProvider } from "./src/hooks/processes_context.js";
+export { useProcess, ProcessProvider } from "./src/hooks/process_context.js";
+export { CircularProgressWithContent } from "./src/lib/circular-progress-with-content.js";
