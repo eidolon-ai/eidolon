0b0f6207df6d0db76db9948cdfaa91cc33f55fc1
Jacob Habib <jahgoreds@gmail.com>
2024-06-19 00:46:02 -0500

    cleanup

--- webui/.eslintrc.js
+++ webui/.eslintrc.js
@@ -1,3 +1,4 @@
+// This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
--- webui/apps/eidolon-ui2/jest.config.js
+++ webui/apps/eidolon-ui2/jest.config.js
@@ -1,7 +1,7 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.mjs and .env files in your test environment
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
 
