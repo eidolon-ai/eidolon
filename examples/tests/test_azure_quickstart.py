b107ff5a7ca9012056641ead6e87eb5a23f5f97d
Luke Lalor <lukehlalor@gmail.com>
2024-09-18 10:30:41 -0700

    maketests like all the others

--- examples/tests/test_azure_quickstart.py
+++ examples/tests/test_azure_quickstart.py
@@ -1,24 +1,20 @@
 import pytest
 
 from eidolon_ai_client.client import Agent
-from eidolon_ai_sdk.system.resources.resources_base import load_resources
-from eidolon_ai_sdk.test_utils.machine import TestMachine
-from eidolon_ai_sdk.test_utils.server import serve_thread
-
-
-@pytest.fixture(scope="module")
-def machine(tmp_path_factory):
-    return TestMachine(tmp_path_factory.mktemp("test_utils_storage"))
 
 
 @pytest.fixture(scope="module", autouse=True)
-def server(eidolon_examples, machine):
-    resources = load_resources(eidolon_examples / "azure_quickstart" / "resources")
-    with serve_thread([machine, *resources]):
-        yield
+def http_server(eidolon_server, eidolon_examples):
+    with eidolon_server(eidolon_examples / "azure_quickstart" / "resources", log_file="azure_quickstart.txt") as server:
+        yield server
 
 
-async def test_can_hit_generic_agent(server_loc):
-    process = await Agent.get("hello-world").create_process()
+@pytest.fixture
+def agent(server_loc):
+    return Agent(machine=server_loc, agent="hello-world")
+
+
+async def test_can_hit_generic_agent(agent: Agent):
+    process = await agent.create_process()
     response = await process.action("converse", json="what is the capital of france?")
     assert "paris" in response.data.lower()
