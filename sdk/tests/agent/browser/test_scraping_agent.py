f8bf5fd0660d86d7ffaeb83bb92c49b007ba28c3
Luke Lalor <lukehlalor@gmail.com>
2024-10-23 20:43:20 -0700

    fix some tests, delete is per resource type not instance

--- sdk/tests/agent/browser/test_scraping_agent.py
+++ sdk/tests/agent/browser/test_scraping_agent.py
@@ -8,28 +8,29 @@ from eidolon_ai_sdk.system.resources.resources_base import Metadata
 from eidolon_ai_sdk.util.class_utils import fqn
 
 
-class TestScrapingAgent:
-    @pytest.fixture(scope="class")
-    def scraper_agent(self, test_dir):
-        return AgentResource(
-            apiVersion="eidolon/v1",
-            metadata=Metadata(name="ScrapingAgent"),
-            spec=Reference(
-                implementation=fqn(WebScrapingAgent),
-                name="sa",
-                description="A test retriever agent",
-            ),
-        )
+@pytest.fixture(scope="module")
+def scraper_agent(test_dir):
+    return AgentResource(
+        apiVersion="eidolon/v1",
+        metadata=Metadata(name="ScrapingAgent"),
+        spec=Reference(
+            implementation=fqn(WebScrapingAgent),
+            name="sa",
+            description="A test retriever agent",
+        ),
+    )
 
-    @pytest.fixture(scope="class")
-    async def agent(self, scraper_agent, run_app) -> Agent:
-        async with run_app(scraper_agent):
-            yield Agent.get("ScrapingAgent")
 
-    async def test_get_page_content(self, agent):
-        process = await agent.create_process()
-        found = await process.action("getPageContent", body={"url": "https://www.eidolonai.com"})
-        # noinspection Pydantic
-        found = WebScrapeResponse.model_validate(found.data)
-        assert "Eidolon AI" in found.text
-        assert "https://www.eidolonai.com/blog" in found.links
+@pytest.fixture(scope="module")
+async def agent(scraper_agent, run_app) -> Agent:
+    async with run_app(scraper_agent):
+        yield Agent.get("ScrapingAgent")
+
+
+async def test_get_page_content(agent):
+    process = await agent.create_process()
+    found = await process.action("getPageContent", body={"url": "https://www.eidolonai.com"})
+    # noinspection Pydantic
+    found = WebScrapeResponse.model_validate(found.data)
+    assert "Eidolon AI" in found.text
+    assert "https://www.eidolonai.com/blog" in found.links
