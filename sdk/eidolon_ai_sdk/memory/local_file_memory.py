c5939fbd0c8bd7ad147ab18c1a91e7e99b27787e
Luke Lalor <lukehlalor@gmail.com>
2024-11-06 13:24:15 -0800

    Update local_file_memory.py

--- sdk/eidolon_ai_sdk/memory/local_file_memory.py
+++ sdk/eidolon_ai_sdk/memory/local_file_memory.py
@@ -16,8 +16,8 @@ class LocalFileMemoryConfig(BaseModel):
     Retrieve documents from a local file system. Note that this is the file system where the Eidolon application is
     running. This means building the files onto the runtime image, or mounting a volume to the container.
 
-    [Build the files into the runtime image](https://docs.docker.com/reference/dockerfile/#copy)
     [Docker volume](https://docs.docker.com/engine/storage/volumes/#use-a-volume-with-docker-compose)
+    [Build the files into the runtime image](https://docs.docker.com/reference/dockerfile/#copy)
     [Kubernetes volumes](https://kubernetes.io/docs/concepts/storage/volumes/)
     """
 
