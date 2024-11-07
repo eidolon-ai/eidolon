8c523540f7275160983199fbb0c7bddf76a33aac
abhi-vachani <abhi.vachani.dev@gmail.com>
2024-11-06 22:14:54 -0500

    getting tools and testing

--- sdk/eidolon_ai_sdk/builtins/logic_units/toolhouse_logic_unit.py
+++ sdk/eidolon_ai_sdk/builtins/logic_units/toolhouse_logic_unit.py
@@ -8,9 +8,9 @@ from pydantic import BaseModel, Field
 
 from eidolon_ai_sdk.apu.logic_unit import LogicUnit, llm_function
 from eidolon_ai_sdk.system.reference_model import Specable
-from sdk.eidolon_ai_sdk.system.tool_builder import ToolUnit
+from eidolon_ai_sdk.system.tool_builder import ToolBuilder
 
-class Toolhouse(ToolUnit):
+class Toolhouse(ToolBuilder):
     """A configurable tool backed by Toolhouse.ai that can be added to Eidolon Agents"""
     ## At a high level, here's what we need:
     ## 2. Toolhouse API_KEY
@@ -29,10 +29,14 @@ def tool_build(spec: Toolhouse):
         th.set_base_url(spec.base_url)
 
     tools = th.get_tools()
-    breakpoint()
+    
+    #breakpoint()
 
     for tool in tools:
-        ## Register tool?
+        @Toolhouse.tool(description=tool['function']['description'], name=tool['function']['name'], input_schema=tool['function']['parameters'])
+        async def tool_register(**kwargs):
+            print(kwargs) ## Need to execute toolhouse request and return the response (Run the tool), set up another test that sets up agent that uses tool, look @ test_tool_builder to see how to define agent to run server with, talking to agent and having it use one of the tools
+        ## Might have agent set up first to debug 
 
 
     ## Loop through tools (prob array of tools) Register it on toolhouse class @Toolhouse.tool (name, desc, json), function that i register, is supposed to run the tool w/ proper arguments 
@@ -45,6 +49,9 @@ def tool_build(spec: Toolhouse):
     ## So ... Get each tool, register it, and run it? What to do with response?
     ## Note: Check tool_builder and test_tool_builder for reference.
     ## Get pytest running and debugger --> Check out docs and Luke's code for this
+
+
+    ##
 
    
 
--- sdk/tests/builtins/logic_units/test_toolhouse.py
+++ sdk/tests/builtins/logic_units/test_toolhouse.py
@@ -1,7 +1,12 @@
-from sdk.eidolon_ai_sdk.builtins.logic_units.toolhouse_logic_unit import Toolhouse, tool_build
+from eidolon_ai_sdk.builtins.logic_units.toolhouse_logic_unit import Toolhouse, tool_build
 
 
-def test_tool_registration():
-    config = Toolhouse()
-    tool_build(config)
+async def test_tool_registration():
+    tools = await Toolhouse().build_tools(None)
+    assert len(tools)==3
+
+    tool_names = [t.name for t in tools]
+    assert tool_names==['code_interpreter', 'exa_web_search', 'scraper']
+
+
 
