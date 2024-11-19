f9041bd132cc25dcfb79fba57d3ee016d02f0eaf
Luke Lalor <lukehlalor@gmail.com>
2024-11-19 15:59:18 -0800

    tests passing

--- sdk/tests/agent/test_retriever_agent.py
+++ sdk/tests/agent/test_retriever_agent.py
@@ -1,5 +1,7 @@
+import os
 from os import environ
 
+import certifi
 import pytest
 
 from eidolon_ai_client.client import Agent
@@ -134,16 +136,16 @@ class TestRetrieverAgent:
         await process.action("search", body={"question": "what about a .exe"})
 
     async def test_azure(self, agent):
-        s3_agent = Agent.get("RetrieverAgentAzure")
-        process = await s3_agent.create_process()
+        azure_agent = Agent.get("RetrieverAgentAzure")
+        process = await azure_agent.create_process()
         found = await process.action("search", body={"question": "how do I make a pdf?"})
         assert "Get_Started_With_Smallpdf" in str(found.data)
 
     async def test_azure_cached_results(self, agent):
-        s3_agent = Agent.get("RetrieverAgentAzure")
-        process = await s3_agent.create_process()
+        azure_agent = Agent.get("RetrieverAgentAzure")
+        process = await azure_agent.create_process()
         await process.action("search", body={"question": "how do I make a pdf?"})
 
         #  will error if there is an issue with syncing docs
-        process = await s3_agent.create_process()
+        process = await azure_agent.create_process()
         await process.action("search", body={"question": "what about a .exe"})
